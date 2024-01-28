bced3e73e73cb2329b732769361db8bbe34d794d
Khairul-Anam-Mubin <anam.mubin1999@gmail.com>
2024-01-28 09:58:39 +0600

    indent fix

--- Chat.Contact.Application/Commands/CreateNewGroupCommand.cs
+++ Chat.Contact.Application/Commands/CreateNewGroupCommand.cs
@@ -1,16 +1,15 @@
 ﻿using Chat.Framework.CQRS;
 
-namespace Chat.Contact.Application.Commands
-{
-    public class CreateNewGroupCommand : ICommand
-    {
-        public string GroupName { get; set; }
-        public string CreatedBy { get; set; }
+namespace Chat.Contact.Application.Commands;
 
-        public CreateNewGroupCommand(string groupName, string createdBy)
-        {
-            GroupName = groupName;
-            CreatedBy = createdBy;
-        }
+public class CreateNewGroupCommand : ICommand
+{
+    public string GroupName { get; set; }
+    public string CreatedBy { get; set; }
+
+    public CreateNewGroupCommand(string groupName, string createdBy)
+    {
+        GroupName = groupName;
+        CreatedBy = createdBy;
     }
 }
