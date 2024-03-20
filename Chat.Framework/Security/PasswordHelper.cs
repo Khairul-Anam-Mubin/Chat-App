569cd4600b22da3e3e658f87e1abfedc7d66f806
Khairul-Anam-Mubin <anam.mubin1999@gmail.com>
2024-03-20 06:19:17 +0600

    password salting ongoing

---
+++ Chat.Framework/Security/PasswordHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Framework.Security;
+
+public class PasswordHelper
+{
+    public static string GenerateRandomSalt(int keySize)
+    {
+        return RandomNumberGenerator.GetHexString(keySize);
+    }
+
+    public static string GetPasswordHash(string password, string salt)
+    {
+        var passwordHash = CheckSumGenerator.GetCheckSum(password, salt);
+        return passwordHash;
+    }
+}
