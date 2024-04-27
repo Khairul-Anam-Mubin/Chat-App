854201ec6460af88033d5fa44486e4a0e73b7f35
Khairul-Anam-Mubin <anam.mubin1999@gmail.com>
2024-04-27 11:23:32 +0600

    nuget updated

--- Chat.Infrustructure.Shared/Configurations/RabbitMqInstaller.cs
+++ Chat.Infrustructure.Shared/Configurations/RabbitMqInstaller.cs
@@ -1,8 +1,5 @@
-﻿using KCluster.Framework;
-using KCluster.Framework.Extensions;
-using KCluster.Framework.MessageBrokers;
+﻿using KCluster.Framework.MessageBrokers;
 using KCluster.Framework.ServiceInstaller;
-using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,32 +9,6 @@ public class RabbitMqInstaller : IServiceInstaller
 {
     public void Install(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddSingleton(configuration.TryGetConfig<MessageBrokerConfig>());
-        services.AddTransient<IEventBus, EventBus>();
-        services.AddTransient<ICommandBus, CommandBus>();
-        services.AddTransient<IMessageRequestClient, MessageRequestClient>();
-
-        services.AddMassTransit(busConfigurator =>
-        {
-            busConfigurator.SetDefaultEndpointNameFormatter();
-
-            AssemblyCache.Instance.GetAddedAssemblies().ForEach(assembly =>
-                busConfigurator.AddConsumers(assembly));
-
-            busConfigurator.UsingRabbitMq((context, configurator) =>
-            {
-                var messageBrokerConfig = context.GetRequiredService<MessageBrokerConfig>();
-
-                configurator.Host(new Uri(messageBrokerConfig.Host), hostConfigurator =>
-                {
-                    hostConfigurator.Username(messageBrokerConfig.UserName);
-                    hostConfigurator.Password(messageBrokerConfig.Password);
-                });
-
-                configurator.AutoDelete = true;
-
-                configurator.ConfigureEndpoints(context);
-            });
-        });
+        services.AddRabbitMqMassTransit(configuration);
     }
 }
